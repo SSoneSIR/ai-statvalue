bc0975755c09866967645e92f3b8a1d9619a3857
Yashaswan Gautam <yashaswanyashu@gmail.com>
2025-04-25 16:34:21 +0545

    Remaining file

--- backend/pred/urls.py
+++ backend/pred/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [  
     path('players/', views.search_players, name='search_players'),  
     path('predict/', views.generate_prediction, name='generate_prediction'),
+    path('player-history/<str:player_name>/', views.player_history, name='player_history'),
 
     # path('api/players/', views.get_players, name='player-list'),
     # path('api/predict/', views.predict_market_value, name='predict'),
